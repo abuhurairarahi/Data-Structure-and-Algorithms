8cf3790f310cac147a154d6526b67a35c38c1119
abuhurairarahi <mabuhurairarahi@gmail.com>
2026-07-10 18:31:55 +0600

    LinkedList added

---
+++ LinkedList/LinkedList.cpp
@@ -0,0 +1,33 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+struct listNode
+{
+    int data;
+    listNode* nextAddress;
+};
+
+listNode* createnode (int data)
+{
+    listNode* tempNode = (listNode*)malloc(sizeof(listNode));
+    //default datatype of malloc (void*)
+    tempNode->data = data;
+    tempNode->nextAddress = NULL;
+    
+    return tempNode;
+}
+
+int main()
+{
+    listNode* nodeA = createnode(1);
+    listNode* nodeB = createnode(2);
+
+    //Access node data
+    printf("%d\n", nodeA->data);
+
+    //Connect nodes
+    nodeA->nextAddress = nodeB;
+    printf("%d", nodeA->nextAddress->data);
+
+    return 0;
+}
---
+++ LinkedList/Node Insertion/Insert_last.cpp
@@ -0,0 +1,70 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+struct listNode
+{
+    int data;
+    listNode* nextAddress;
+};
+
+//tracker initialization
+listNode* head = NULL;
+listNode* tail = NULL;
+
+listNode* createnode (int data)
+{
+    listNode* tempNode = (listNode*)malloc(sizeof(listNode));
+    tempNode->data = data;
+    tempNode->nextAddress = NULL;
+
+    return tempNode;
+}
+
+void insertLast (int data)
+{
+    if(head == NULL)
+    {
+        listNode* tempNode = createnode(data);
+
+        //assigning head & tail 
+        head = tempNode;
+        tail = tempNode;
+    
+        //nodeA -> NULL (h=t=nodeA)
+    }
+    else
+    {
+        listNode* tempNode = createnode(data);
+        //connecting the newest node with the existing tail
+        tail->nextAddress = tempNode;
+        //re-assigning the tail
+        tail = tempNode;
+    
+        //nodeA -> nodeB -> NULL (h=nodeA t=nodeB)
+        //nodeA -> nodeB -> nodeC (h=nodeA t=nodeC)
+    }
+}
+
+int main()
+{   
+    //Using this method: Time Complexity becomes O(1) 
+    insertLast(10);
+    insertLast(20);
+    insertLast(30);
+    insertLast(40);
+    insertLast(50);
+
+    // listNode* iteration = head;
+    // while(iteration != NULL)
+    // {
+    //     printf("%d ", iteration->data);
+    //     iteration = iteration->nextAddress;
+    // }
+
+    for(listNode* i = head; i != NULL; i=i->nextAddress)
+    {
+        printf("%d ", i->data);
+    }
+
+    return 0;
+}
